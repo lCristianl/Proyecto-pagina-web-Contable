f276bd6fb88f7b9354a95e8f0a87699db01d3f8c
lCristianl <cjortega03@tecnologicosudamericano.edu.ec>
2025-08-15 11:04:19 -0500

    Nuevo parámetro para Peso
    Se añadió un nuevo parámetro que sirve para guardar el peso unitario de cada producto

--- crud_contable/models.py
+++ crud_contable/models.py
@@ -34,6 +34,7 @@ class Product(models.Model):
     type = models.CharField(max_length=10, choices=PRODUCT_TYPE_CHOICES)
     code = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
+    unit_weight = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
